b42b86d5b39c86af326e5a8d50058f4d7441d8b0
Melih Can AYDIN <melihcanaydin@gmail.com>
2024-11-26 01:23:08 +0200

    Logger and CompareContacts

---
+++ src/utils/CompareContacts.ts
@@ -0,0 +1,25 @@
+import { Contact } from "../models/Contact";
+
+const CompareContacts = (mailchimpContacts: Contact[], pipedriveContacts: Contact[]) => {
+    // Filter contacts present only in Mailchimp
+    const mailchimpOnly = mailchimpContacts.filter(
+        (mailchimpContact) =>
+            !pipedriveContacts.some(
+                (pipedriveContact) =>
+                    pipedriveContact.emailAddress === mailchimpContact.emailAddress
+            )
+    );
+
+    // Filter contacts present only in Pipedrive
+    const pipedriveOnly = pipedriveContacts.filter(
+        (pipedriveContact) =>
+            !mailchimpContacts.some(
+                (mailchimpContact) =>
+                    mailchimpContact.emailAddress === pipedriveContact.emailAddress
+            )
+    );
+
+    return { mailchimpOnly, pipedriveOnly }; // Return comparison result
+};
+
+export { CompareContacts };
