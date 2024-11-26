a15ece427922c16d1a3418b3b63828ac351ede23
Melih Can AYDIN <melihcanaydin@gmail.com>
2024-11-26 16:17:04 +0200

    UnifiedService getch contacts from multiple platforms

--- src/domain/services/UnifiedContactService.ts
+++ src/domain/services/UnifiedContactService.ts
@@ -1,24 +1,13 @@
-import { ContactFetcher } from "../fetchers/ContactFetcher";
-import { PipedriveFetcher } from "../fetchers/PipedriveFetcher";
-import { MailchimpFetcher } from "../fetchers/MailchimpFetcher";
-import { Contact } from "../../domain/models/Contact";
+import { getMailchimpContacts } from "./MailchimpService";
+import { getPipedriveContacts } from "./PipedriveService";
 
-export class UnifiedContactService {
-    private fetchers: Record<string, ContactFetcher>;
-
-    constructor() {
-        this.fetchers = {
-            pipedrive: new PipedriveFetcher(),
-            mailchimp: new MailchimpFetcher(),
-        };
-    }
-
-    async fetchContacts(platform: string): Promise<Contact[]> {
-        const fetcher = this.fetchers[platform.toLowerCase()];
-        if (!fetcher) {
-            throw new Error(`Unsupported platform: ${platform}`);
-        }
-
-        return await fetcher.fetchContacts();
-    }
-}
+export const fetchContactsFromPlatform = async (platform: string) => {
+  switch (platform) {
+    case "mailchimp":
+      return getMailchimpContacts();
+    case "pipedrive":
+      return getPipedriveContacts();
+    default:
+      throw new Error(`Unsupported platform: ${platform}`);
+  }
+};
