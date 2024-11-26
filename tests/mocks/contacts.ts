b23fcfb3e19dc82b4475efea1bc8c77f10903b07
Melih Can AYDIN <melihcanaydin@gmail.com>
2024-11-26 15:19:01 +0200

    mock data

---
+++ tests/mocks/contacts.ts
@@ -0,0 +1,35 @@
+import { Contact } from "./../../src/models/Contact";
+
+export const mockMailchimpContacts: Contact[] = [
+  {
+    emailAddress: "a@mail.com",
+    phoneNumber: "1234567",
+    source: "mailchimp",
+    rating: null,
+    contactDateAdded: "2024-11-25T10:00:00Z",
+  },
+  {
+    emailAddress: "b@mail.com",
+    phoneNumber: "7654321",
+    source: "mailchimp",
+    rating: null,
+    contactDateAdded: "2024-11-25T10:00:00Z",
+  },
+];
+
+export const mockPipedriveContacts: Contact[] = [
+  {
+    emailAddress: "b@mail.com",
+    phoneNumber: "9876543",
+    source: "pipedrive",
+    rating: null,
+    contactDateAdded: "2024-11-25T10:00:00Z",
+  },
+  {
+    emailAddress: "c@mail.com",
+    phoneNumber: "5566778",
+    source: "pipedrive",
+    rating: null,
+    contactDateAdded: "2024-11-25T10:00:00Z",
+  },
+];
